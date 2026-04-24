2e9afe07c2b4b6d4ce2dc8ffb1a3cbe5f8c77edb
GigiRighi <giovana.righi@aluno.senai.br>
2026-04-24 13:39:01 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -96,6 +96,30 @@ function desligarLampadaSala3() {
 }
 
 
+function ligarLampadaSala4() {
+
+    document.getElementById("lp-quarto2").classList.add('acesa')
+
+    const msg = new Paho.MQTT.Message('')
+    msg.destinationName = 'senai510/lampada/quarto2/ligar'
+    clienteWeb.send(msg)
+
+}
+
+
+
+function desligarLampadaSala4() {
+    document.getElementById("lp-quarto2").classList.remove('acesa')
+
+    const msg = new Paho.MQTT.Message('')
+    msg.destinationName = 'senai510/lampada/quarto2/desligar'
+    clienteWeb.send(msg)
+
+}
+
+
+
+
 
 function ligarTudo() {
 
@@ -122,3 +146,65 @@ function desligarTudo() {
     msg.destinationName = 'senai510/lampada/desligar'
     clienteWeb.send(msg)
 }
+
+
+
+let alarmeAtivo = false
+
+function alarmePiscando() {
+
+    if (alarmeAtivo) return
+    alarmeAtivo = true
+
+    const lampadas = ["lp-sala", "lp-cozinha", "lp-quarto1", "lp-quarto2"]
+
+    let ligado = false
+
+    const intervalo = setInterval(() => {
+
+        ligado = !ligado
+
+        // Atualiza interface
+        lampadas.forEach(id => {
+            const el = document.getElementById(id)
+            if (el) {
+                el.classList.toggle('acesa', ligado)
+            }
+        })
+
+        // 🔥 ENVIA MQTT
+        const msg = new Paho.MQTT.Message('')
+        msg.destinationName = ligado 
+            ? 'senai510/lampada/ligar'
+            : 'senai510/lampada/desligar'
+
+        clienteWeb.send(msg)
+
+    }, 400)
+
+    setTimeout(() => {
+        clearInterval(intervalo)
+        alarmeAtivo = false
+
+        // Desliga tudo no final
+        lampadas.forEach(id => {
+            const el = document.getElementById(id)
+            if (el) el.classList.remove('acesa')
+        })
+
+        // 🔥 garante que ESP32 também desliga
+        const msg = new Paho.MQTT.Message('')
+        msg.destinationName = 'senai510/lampada/desligar'
+        clienteWeb.send(msg)
+
+    }, 5000)
+}
+
+
+document.querySelectorAll("button").forEach(btn => {
+    btn.addEventListener("click", () => {
+        if ("vibrate" in navigator) {
+            navigator.vibrate(100)
+        }
+    })
+})
